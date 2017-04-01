a2697ecc140228f6f5719e8a100ff175d48c70e3
Henry <minglei-henry.hei@schneider-electric.com>
2017-04-01 17:11:48 +0800

    temp save.

--- cmd/ftpput.go
+++ cmd/ftpput.go
@@ -1,5 +1,3 @@
-// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
-//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -28,11 +26,11 @@ import (
 	"time"
 
 	"github.com/dutchcoders/goftp"
+	ui "github.com/gizak/termui"
 	"github.com/mythay/anet/util"
 )
 
 var fUser, fPasswd, fDestdir, fLocalfile string
-var progress = uiprogress.New()
 
 // ftpputCmd represents the put command
 var ftpputCmd = &cobra.Command{
@@ -51,93 +49,13 @@ For example:
 		if err != nil {
 			return err
 		}
-		nips := len(ips)
+
 		if _, err := os.Stat(fLocalfile); err != nil {
 			return fmt.Errorf("local file '%s' not exist", fLocalfile)
 		}
 
-		// try to create all the task
-		allprogress := make([]simpleUIBind, nips)
-		wg := sync.WaitGroup{}
-		wg.Add(nips)
-		errmsg := make(chan error, nips)
-		for i := range allprogress {
-			allprogress[i].upload, err = newFileUpload(&ftpServerInfo{
-				user:   fUser,
-				passwd: fPasswd,
-				ipaddr: ips[i]},
-				fLocalfile,
-				fDestdir)
-			allprogress[i].bar = progress.AddBar(100).AppendCompleted().PrependElapsed()
-			allprogress[i].bar.PrependFunc(func(f *fileUpload) uiprogress.DecoratorFunc {
-				return func(b *uiprogress.Bar) string {
-
-					if f == nil {
-						return fmt.Sprintf("%s :OPEN :ERROR", f.server.ipaddr.String())
-					}
-					if f.err != nil {
-						return fmt.Sprintf("%s :%s :ERROR", f.server.ipaddr.String(), f.status)
-					}
-					return fmt.Sprintf("%s :%s :     ", f.server.ipaddr.String(), f.status)
-
-				}
-			}(allprogress[i].upload))
-		}
-
-		progress.Start()
-
-		for _, item := range allprogress {
-			go func(s simpleUIBind) {
-				var err error
-				defer wg.Done()
-				err = s.upload.upload()
-				if err != nil {
-					errmsg <- err
-				}
-			}(item)
-			time.Sleep(time.Millisecond * 100)
-		}
-
-		refreshTimer := time.NewTimer(time.Millisecond * 100)
-		go func() {
-			refreshTimer.Reset(time.Millisecond * 100)
-			_, ok := <-refreshTimer.C
-			if !ok {
-				return
-			}
-			for _, item := range allprogress {
-				f := item.upload
-				bar := item.bar
-				if bar.Current() == 100 || f.err != nil { // all done, no need to update anymore
-					break
-				}
-				if f.size == 0 { // empty file,
-					if f.status == "DONE" {
-						bar.Set(99)
-						bar.Incr()
-					}
-
-				} else {
-					percent := f.offset * 100 / f.size
-					bar.Set(percent - 1)
-					bar.Incr()
-				}
-			}
-		}()
-
-		wg.Wait()
-		refreshTimer.Stop()
-		progress.Stop()
-		errcount := 0
-
-		close(errmsg)
-		for msg := range errmsg {
-			fmt.Println(msg)
-			errcount++
-		}
-
-		fmt.Printf("\nSTATISTIC: %d/%d success\n", nips-errcount, nips)
-		return nil
+		// return uploadSimpleView(ips)
+		return uploadDashView(ips)
 	},
 }
 
@@ -147,7 +65,6 @@ func init() {
 	ftpputCmd.Flags().StringVarP(&fPasswd, "password", "p", "pcfactory", "Correspoding ftp password")
 	ftpputCmd.Flags().StringVarP(&fDestdir, "directory", "d", "/fw", "Ftp server directory to store the file")
 	ftpputCmd.Flags().StringVarP(&fLocalfile, "localfile", "l", "App2.out", "local file path to be uploaded")
-	progress.Width = 40
 
 }
 
@@ -168,6 +85,175 @@ type fileUpload struct {
 	lock    sync.Mutex
 }
 
+func uploadSimpleView(ips []net.IP) error {
+	// try to create all the task
+	var progress = uiprogress.New()
+	progress.Width = 40
+	nips := len(ips)
+	allprogress := make([]simpleUIBind, nips)
+	wg := sync.WaitGroup{}
+	wg.Add(nips)
+	errmsg := make(chan error, nips)
+	for i := range allprogress {
+		allprogress[i].upload, _ = newFileUpload(&ftpServerInfo{
+			user:   fUser,
+			passwd: fPasswd,
+			ipaddr: ips[i]},
+			fLocalfile,
+			fDestdir)
+		allprogress[i].bar = progress.AddBar(100).AppendCompleted().PrependElapsed()
+		allprogress[i].bar.PrependFunc(func(f *fileUpload) uiprogress.DecoratorFunc {
+			return func(b *uiprogress.Bar) string {
+				if f == nil {
+					return fmt.Sprintf("%-15s :OPEN :ERROR", f.server.ipaddr.String())
+				}
+
+				if f.err != nil {
+					return fmt.Sprintf("%-15s :%-8s :ERROR", f.server.ipaddr.String(), f.status)
+				}
+				if b.Current() != 100 {
+					if f.size == 0 { // empty file,
+						if f.status == "DONE" {
+							b.Set(99)
+							b.Incr()
+						}
+
+					} else {
+						percent := f.offset * 100 / f.size
+						b.Set(percent - 1)
+						b.Incr()
+					}
+				}
+				return fmt.Sprintf("%-15s :%-8s       ", f.server.ipaddr.String(), f.status)
+
+			}
+		}(allprogress[i].upload))
+	}
+	fmt.Println(len(allprogress))
+	progress.Start()
+
+	for _, item := range allprogress {
+		go func(s simpleUIBind) {
+			var err error
+			defer wg.Done()
+			err = s.upload.upload()
+			if err != nil {
+				errmsg <- err
+			}
+		}(item)
+		time.Sleep(time.Millisecond * 10)
+	}
+
+	wg.Wait()
+	time.Sleep(time.Millisecond * 200)
+	progress.Stop()
+	errcount := 0
+
+	close(errmsg)
+	for _ = range errmsg {
+		errcount++
+	}
+
+	fmt.Printf("\nSTATISTIC: %d/%d success\n", nips-errcount, nips)
+	return nil
+}
+
+func uploadDashView(ips []net.IP) error {
+	err := ui.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer ui.Close()
+
+	g := ui.NewGauge()
+	g.Percent = 50
+	g.Height = 3
+
+	g.BorderLabel = "Gauge"
+	g.BarColor = ui.ColorRed
+	g.BorderFg = ui.ColorWhite
+	g.BorderLabelFg = ui.ColorCyan
+	ui.Body.AddRows(
+		ui.NewRow(
+			ui.NewCol(12, 0, g)))
+	nips := len(ips)
+	allprogress := make([]simpleUIBind, nips)
+	wg := sync.WaitGroup{}
+	wg.Add(nips)
+	errmsg := make(chan error, nips)
+	for i := range allprogress {
+		allprogress[i].upload, _ = newFileUpload(&ftpServerInfo{
+			user:   fUser,
+			passwd: fPasswd,
+			ipaddr: ips[i]},
+			fLocalfile,
+			fDestdir)
+		allprogress[i].bar = progress.AddBar(100).AppendCompleted().PrependElapsed()
+		allprogress[i].bar.PrependFunc(func(f *fileUpload) uiprogress.DecoratorFunc {
+			return func(b *uiprogress.Bar) string {
+				if f == nil {
+					return fmt.Sprintf("%-15s :OPEN :ERROR", f.server.ipaddr.String())
+				}
+
+				if f.err != nil {
+					return fmt.Sprintf("%-15s :%-8s :ERROR", f.server.ipaddr.String(), f.status)
+				}
+				if b.Current() != 100 {
+					if f.size == 0 { // empty file,
+						if f.status == "DONE" {
+							b.Set(99)
+							b.Incr()
+						}
+
+					} else {
+						percent := f.offset * 100 / f.size
+						b.Set(percent - 1)
+						b.Incr()
+					}
+				}
+				return fmt.Sprintf("%-15s :%-8s       ", f.server.ipaddr.String(), f.status)
+
+			}
+		}(allprogress[i].upload))
+	}
+	fmt.Println(len(allprogress))
+	progress.Start()
+
+	for _, item := range allprogress {
+		go func(s simpleUIBind) {
+			var err error
+			defer wg.Done()
+			err = s.upload.upload()
+			if err != nil {
+				errmsg <- err
+			}
+		}(item)
+		time.Sleep(time.Millisecond * 10)
+	}
+
+	wg.Wait()
+	time.Sleep(time.Millisecond * 200)
+	progress.Stop()
+	errcount := 0
+
+	close(errmsg)
+	for _ = range errmsg {
+		errcount++
+	}
+
+	fmt.Printf("\nSTATISTIC: %d/%d success\n", nips-errcount, nips)
+	// calculate layout
+	ui.Body.Align()
+
+	ui.Render(ui.Body)
+
+	ui.Render(g) // feel free to call Render, it's async and non-block
+	ui.Handle("/sys/kbd/q", func(ui.Event) {
+		ui.StopLoop()
+	})
+	ui.Loop()
+	return nil
+}
 func newFileUpload(server *ftpServerInfo, localfile string, destdir string) (*fileUpload, error) {
 	fileInfo, err := os.Stat(localfile)
 	if err != nil {
